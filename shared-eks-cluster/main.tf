a278cee633db5e042aaa24ddb19cfeb13940ffd2
Darren Pham <phamdarren1@gmail.com>
2023-07-23 22:30:05 -0700

    main.tf, change region to us-west-2 as its cheapre

--- shared-eks-cluster/main.tf
+++ shared-eks-cluster/main.tf
@@ -50,7 +50,7 @@ locals {
 
   name = "${local.application}-${local.environment}"
 
-  region   = "us-west-1"
+  region   = "us-west-2"
   vpc_cidr = "10.1.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 2)
 
