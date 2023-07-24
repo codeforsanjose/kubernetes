78825382f1c162f906fb619996015ac4dab9386f
Darren Pham <phamdarren1@gmail.com>
2023-07-24 05:36:09 -0700

    `happeningatm-prod` infra deploy

---
+++ deployments/happeningatm/global/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = ">= 1.5.3"
+
+  backend "s3" {
+    bucket = "opensourcesanjose-terraform"
+    key    = "happeningatm/global.tfstate"
+    region = "us-west-1"
+    # dynamodb_table = "terraform-state-lock-dynamo"
+  }
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    doppler = {
+      source = "DopplerHQ/doppler"
+    }
+  }
+}
+
+provider "aws" {
+  region = local.region
+}
+
+locals {
+  application = "happeningatm"
+  ecr_respositories = ["fronted", "backend", "graphql"]
+
+  region = "us-west-2"
+
+  tags = {
+    application       = local.application
+    terraform-managed = "True"
+  }
+}
